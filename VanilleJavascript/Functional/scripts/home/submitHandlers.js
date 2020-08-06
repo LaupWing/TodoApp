fe97d47f8276667fbee1e2ffbb61e7115b9258a1
LaupWing <loc_strict@hotmail.com>
2020-08-06 18:55:22 +0200

    a check added

--- VanilleJavascript/Functional/scripts/home/submitHandlers.js
+++ VanilleJavascript/Functional/scripts/home/submitHandlers.js
@@ -4,6 +4,13 @@ export const addTodoSubmitHandler =  ()=>{
     document.querySelector('form').addEventListener('submit',async (e)=>{
         e.preventDefault();
         const todo = e.target.todo.value;
+        const exists = Array
+            .from(document.querySelectorAll('.todo'))
+            .map(x=>x.textContent.trim())
+            .find(x=>x===todo)
+        if(exists){
+            return alert('Todo already exists');
+        }
         addTodo(todo); 
         e.target.todo.value = '';       
     });
