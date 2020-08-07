478ca98a87272806f2b3811d40f80fd579b8b46e
LaupWing <loc_strict@hotmail.com>
2020-08-07 15:29:18 +0200

    e

--- VanilleJavascript/ObjectOriented/scripts/app.js
+++ VanilleJavascript/ObjectOriented/scripts/app.js
@@ -1,11 +1,18 @@
 import './db/db.js';
 import Auth from './Auth/Auth.js';
 
-firebase.auth().onAuthStateChanged(user=>{
-    if(!user){
-        new Auth();
-    }else{
-        console.log('home screen')
+class App{
+    constructor(){
+        this.active = null;
+        firebase.auth().onAuthStateChanged(user=>{
+            if(!user){
+                this.active = new Auth();
+            }else{
+                console.log('home screen')
+            }
+        });
     }
-});
+}
+
+new App();
 // firebase.auth().signOut();
