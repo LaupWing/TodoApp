0764c7e77a0fb1b0c850cafe358d13301fe28498
LaupWing <loc_strict@hotmail.com>
2020-08-08 12:01:25 +0200

    split up login form

--- vue_app/src/views/Auth/Auth.vue
+++ vue_app/src/views/Auth/Auth.vue
@@ -2,27 +2,26 @@
     <main class="login">
         <h1>Welcome</h1>
         <div class="form-container">
-            <form class="login_form">
-                <input name='email' type="text" placeholder="Your Email...">
-                <input name='password' type="password" placeholder="Your Password...">
-                <p class="error"></p>
-                <button type="submit">Submit</button>
-                <a href="#">No account? Sign up here!</a>
-            </form>
-            <form class="signup_form hidden">
+            <login-form/>
+            <!-- <form class="signup_form hidden">
                 <input name='email' type="text" placeholder="Your Email...">
                 <input name='password' type="password" placeholder="Your Password...">
                 <input name='password_confirm' type="password" placeholder="Confirm your Password...">
                 <p class="error"></p>
                 <button type="submit">Submit</button>
                 <a href="#">Already hava account? Login here!</a>
-            </form>
+            </form> -->
         </div>
     </main>
 </template>
 
 <script>
+import Login_Form from './Login_Form/Login_Form'
+
 export default {
-    name: 'Auth'
+    name: 'Auth',
+    component:{
+        loginForm: Login_Form
+    }
 }
 </script>
