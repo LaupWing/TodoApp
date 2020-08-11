195c7d500cd5398d7e1e24e46429b17d28658770
LaupWing <loc_strict@hotmail.com>
2020-08-11 20:22:00 +0200

    done i guess

--- php/index.php
+++ php/index.php
@@ -5,57 +5,22 @@
         exit;
     }
     require_once "config.php";
+    print_r($_SESSION);
     $todo = "";
     $errors = array();
 
     if($_SERVER["REQUEST_METHOD"]== "POST"){
         $todo = trim($_POST['todo']);
-        if(empty($email)){
-            $errors[] = 'Email is required';
-        }else{
-            if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
-                $errors[]="$email is not a valid email";
-            }else{
-                $sql = "SELECT id FROM users WHERE email = ?";
-                
-                if($stmt = mysqli_prepare($link, $sql)){
-                    mysqli_stmt_bind_param($stmt, "s", $email);
-
-                    if(mysqli_stmt_execute($stmt)){
-                        mysqli_stmt_store_result($stmt);
-
-                        if(mysqli_stmt_num_rows($stmt) == 1){
-                            $errors[] = "This email is already taken.";
-                        }
-                    } else{
-                        echo "Something went wrong. Please try again later.";
-                    }
-                    mysqli_stmt_close($stmt);
-                }
-            }
-        }
-        if(empty($password)){
-            $errors[] = 'Password is required <br/>';
-        }elseif(strlen($password)<6) {
-            $errors[] = 'Password must have atleast 6 characters';
+        if(empty($todo)){
+            $errors[] = 'Todo cannot be empty';
         }
 
-        if(empty($password_confirm)){
-            $errors[] = "Please confirm password";
-        }else{
-            if(!empty($password_confirm) && !empty($password)){
-                if($password_confirm !== $password){
-                    $errors[] = "Password did not match";
-                }
-            }
-        }
         
         if(empty($errors)){
-            $sql = "INSERT INTO users (email, password) VALUES (?, ?)";
+            $sql = "INSERT INTO todos (todo, owner) VALUES (?, ?)";
 
             if($stmt = mysqli_prepare($link, $sql)){
-                $hashed_password = password_hash($password, PASSWORD_DEFAULT);
-                mysqli_stmt_bind_param($stmt, "ss", $email, $hashed_password);
+                mysqli_stmt_bind_param($stmt, "si", $todo, $_SESSION['id']);
 
                 if(mysqli_stmt_execute($stmt)){
                     header("location: index.php");
