81c281ef9c2cbf9467c593b2a09e98225a86e0ea
LaupWing <loc_strict@hotmail.com>
2020-08-11 20:07:24 +0200

    fixed some bugs

--- php/index.php
+++ php/index.php
@@ -1,10 +1,14 @@
 <?php
-require_once "config.php";
+    session_start();
+    if(!isset($_SESSION["loggedin"])){
+        header("location: login.php");
+        exit;
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
     <?php include('templates/header.php'); ?>
 <body>
-    
+    Home
 </body>
 </html>
