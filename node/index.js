e922770e2d3c7c3899517dcf187a63f3e968df6e
LaupWing <loc_strict@hotmail.com>
2020-08-13 00:30:39 +0200

    static files

--- node/index.js
+++ node/index.js
@@ -10,6 +10,7 @@ const routes = require('./routes/routes.js');
 app
     .use(cors())
     .use(express.urlencoded({extended: false}))
+    .use('/static',express.static(path.join(__dirname, 'public')))
     .use(bodyParser.json({limit: '1mb'}))
     .set('view engine', 'ejs')
     .set('views', path.join(__dirname, 'views'))
