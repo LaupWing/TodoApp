c1a2f037fcb6418bd2a04c14d97fe3e4f3d9f736
LaupWing <loc_strict@hotmail.com>
2020-08-13 19:52:01 +0200

    update

--- django/todos/main/models.py
+++ django/todos/main/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+class User(models.Model):
+    email = models.CharField(200)
+    password= models.CharField(200)
+
 # Create your models here.
+class Todo(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    todo = models.CharField(max_length=300)
+    complete = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.todo
