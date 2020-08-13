d26b76988eef38909444ba3656611c681fd5fc34
LaupWing <loc_strict@hotmail.com>
2020-08-13 02:37:34 +0200

    basic login signup

--- node/routes/routes.js
+++ node/routes/routes.js
@@ -2,10 +2,18 @@ const express   = require('express');
 const router    = new express.Router();
 
 router  
-    .get('/',(req,res)=>{
+    .get('/login',(req,res)=>{
         res.render('template',{
-            page: 'auth'
-        })
+            page: 'login'
+        });
+    })
+    .post('/signup',(req,res)=>{
+        res.redirect('/signup');
+    })
+    .get('/signup',(req,res)=>{
+        res.render('template',{
+            page: 'signup'
+        });
     });
 
 module.exports = router;
