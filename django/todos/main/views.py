34ae126c5253cac3fdc7be495ff79b9334fb9710
LaupWing <loc_strict@hotmail.com>
2020-08-14 12:47:54 +0200

    added some stuff

--- django/todos/main/views.py
+++ django/todos/main/views.py
@@ -4,3 +4,8 @@ from django.http import HttpResponse
 # Create your views here.
 def index(response):
     return render(response, "main/home.html", {})
+
+def login(response):
+    return render(response, "main/login.html", {})
+def signup(response):
+    return render(response, "main/signup.html", {})
