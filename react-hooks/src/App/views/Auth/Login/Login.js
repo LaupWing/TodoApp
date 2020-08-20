97f61abb95ebe633d8929a9d3ed8874c38149733
LaupWing <loc_strict@hotmail.com>
2020-08-20 21:05:11 +0200

    toggle

--- react-hooks/src/App/views/Auth/Login/Login.js
+++ react-hooks/src/App/views/Auth/Login/Login.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-const Login = () => {
+const Login = ({toggle}) => {
     return (
         <form className="login_form">
             <input name='email' type="text" placeholder="Your Email..."/>
             <input name='password' type="password" placeholder="Your Password..."/>
             <p className="error"></p>
             <button type="submit">Submit</button>
-            <p className="link">No account? Sign up here!</p>
+            <p className="link" onClick={()=>toggle()}>No account? Sign up here!</p>
         </form>
     );
 }
--- react-hooks/src/App/views/Auth/Signup/Signup.js
+++ react-hooks/src/App/views/Auth/Signup/Signup.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
-const Signup = () => {
+const Signup = ({toggle}) => {
     return (
-        <form class="signup_form hidden">
+        <form class="signup_form">
             <input name='email' type="text" placeholder="Your Email..."/>
             <input name='password' type="password" placeholder="Your Password..."/>
             <input name='password_confirm' type="password" placeholder="Confirm your Password..."/>
             <p class="error"></p>
             <button type="submit">Submit</button>
-            <p className="link">Already hava account? Login here!</p>
+            <p className="link" onClick={()=>toggle()}>Already hava account? Login here!</p>
         </form>
     );
 }
