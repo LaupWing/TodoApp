fdf05b1ccafb197442efd2d2969d0bca03d6db40
LaupWing <loc_strict@hotmail.com>
2020-08-20 20:03:55 +0200

    login/signuup

--- react-hooks/src/App/views/Auth/Auth.js
+++ react-hooks/src/App/views/Auth/Auth.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Auth = () => {
+    const [login, setLogin] = useState(true);
     return (
-        <div>
-            
-        </div>
+        <main class="login">
+            <h1>Welcome</h1>
+            <div class="form-container">
+                {/* <form class="login_form">
+                    <input name='email' type="text" placeholder="Your Email...">
+                    <input name='password' type="password" placeholder="Your Password...">
+                    <p class="error"></p>
+                    <button type="submit">Submit</button>
+                    <a href="#">No account? Sign up here!</a>
+                </form> */}
+                {/* <form class="signup_form hidden">
+                    <input name='email' type="text" placeholder="Your Email...">
+                    <input name='password' type="password" placeholder="Your Password...">
+                    <input name='password_confirm' type="password" placeholder="Confirm your Password...">
+                    <p class="error"></p>
+                    <button type="submit">Submit</button>
+                    <a href="#">Already hava account? Login here!</a>
+                </form> */}
+            </div>
+        </main>
     );
 }
 
