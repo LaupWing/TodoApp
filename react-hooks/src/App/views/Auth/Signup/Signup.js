e5fc043dca50e605c4f21393066d2d97c54768be
LaupWing <loc_strict@hotmail.com>
2020-08-20 20:07:57 +0200

    singup

---
+++ react-hooks/src/App/views/Auth/Signup/Signup.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Signup = () => {
+    return (
+        <form class="signup_form hidden">
+            <input name='email' type="text" placeholder="Your Email..."/>
+            <input name='password' type="password" placeholder="Your Password..."/>
+            <input name='password_confirm' type="password" placeholder="Confirm your Password..."/>
+            <p class="error"></p>
+            <button type="submit">Submit</button>
+            <a href="#">Already hava account? Login here!</a>
+        </form>
+    );
+}
+
+export default Signup;
