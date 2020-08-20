bf258f71b64486d2d7c3f81aeea596a759060570
LaupWing <loc_strict@hotmail.com>
2020-08-20 21:15:45 +0200

    hmm

--- react-hooks/src/App/views/Auth/Auth.js
+++ react-hooks/src/App/views/Auth/Auth.js
@@ -1,7 +1,8 @@
 import React, {useState} from 'react';
 import Login from './Login/Login';
 import Signup from './Signup/Signup';
-import './Auth.css'
+import './Auth.css';
+import {CSSTransition} from 'react-transition-group';
 
 const Auth = () => {
     const [login, setLogin] = useState(true);
@@ -9,13 +10,17 @@ const Auth = () => {
         <main className="login">
             <h1>Welcome</h1>
             <div className="form-container">
-                {login? 
-                    <Login 
-                        toggle={()=>setLogin(!login)}
-                    /> : 
-                    <Signup
-                        toggle={()=>setLogin(!login)}
-                    />}
+            <CSSTransition in={login} timeout={200} classNames="node"> 
+                <Login 
+                    toggle={()=>setLogin(!login)}
+                /> 
+            </CSSTransition>
+            <CSSTransition in={!login} timeout={200} classNames="node"> 
+                <Signup
+                    toggle={()=>setLogin(!login)}
+                />
+            </CSSTransition>
+
             </div>
         </main>
     );
--- react-hooks/src/App/views/Auth/Signup/Signup.js
+++ react-hooks/src/App/views/Auth/Signup/Signup.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 const Signup = ({toggle}) => {
     return (
-        <form class="signup_form">
+        <form className="signup_form">
             <input name='email' type="text" placeholder="Your Email..."/>
             <input name='password' type="password" placeholder="Your Password..."/>
             <input name='password_confirm' type="password" placeholder="Confirm your Password..."/>
-            <p class="error"></p>
+            <p className="error"></p>
             <button type="submit">Submit</button>
             <p className="link" onClick={()=>toggle()}>Already hava account? Login here!</p>
         </form>
