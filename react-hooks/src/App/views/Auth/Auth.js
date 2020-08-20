1c69d3684382921cd0511b8c69d1b5878171a851
LaupWing <loc_strict@hotmail.com>
2020-08-20 21:17:18 +0200

    reverted

--- react-hooks/src/App/views/Auth/Auth.js
+++ react-hooks/src/App/views/Auth/Auth.js
@@ -2,7 +2,6 @@ import React, {useState} from 'react';
 import Login from './Login/Login';
 import Signup from './Signup/Signup';
 import './Auth.css';
-import {CSSTransition} from 'react-transition-group';
 
 const Auth = () => {
     const [login, setLogin] = useState(true);
@@ -10,17 +9,13 @@ const Auth = () => {
         <main className="login">
             <h1>Welcome</h1>
             <div className="form-container">
-            <CSSTransition in={login} timeout={200} classNames="node"> 
-                <Login 
-                    toggle={()=>setLogin(!login)}
-                /> 
-            </CSSTransition>
-            <CSSTransition in={!login} timeout={200} classNames="node"> 
-                <Signup
-                    toggle={()=>setLogin(!login)}
-                />
-            </CSSTransition>
-
+                {login? 
+                    <Login 
+                        toggle={()=>setLogin(!login)}
+                    /> : 
+                    <Signup
+                        toggle={()=>setLogin(!login)}
+                    />}
             </div>
         </main>
     );
