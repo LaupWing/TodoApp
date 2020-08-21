a54a752b2d345444ad6d2ee6cac22739ef0fb807
LaupWing <loc_strict@hotmail.com>
2020-08-21 18:44:01 +0200

    lol

--- react-hooks/src/App/views/Home/Todos/Todo/Todo.js
+++ react-hooks/src/App/views/Home/Todos/Todo/Todo.js
@@ -3,11 +3,10 @@ import styles from './Todo.module.css';
 
 
 
-const Todo = (props) => {
-    console.log(props)
+const Todo = ({todo}) => {
     return (
         <div className={styles["todo"]}>
-        <h2>{props.todo}</h2>
+        <h2>{todo.todo}</h2>
         <div className={styles["buttons"]}>
             <button className={styles["done"]}>
                 <img src={require('../../../../../assets/done.svg')} alt="toggle"/>
