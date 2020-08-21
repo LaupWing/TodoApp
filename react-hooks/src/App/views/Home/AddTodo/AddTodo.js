1632c38cbff6a429e9a02225ab2cab118674a589
LaupWing <loc_strict@hotmail.com>
2020-08-21 19:19:13 +0200

    set todo works

--- react-hooks/src/App/views/Home/AddTodo/AddTodo.js
+++ react-hooks/src/App/views/Home/AddTodo/AddTodo.js
@@ -1,21 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './AddTodo.module.css';
 
 const AddTodo = ({todos, userCollection}) => {
-    const addingTodo = (e)=>{
-        const todo = e.target.todo.value;
-
-        userCollection().set({
-            todos: [...todos, {
-                done:false,
-                todo
-            }]
-        })
+    const [todo, setTodo] = useState('');
+    const addingTodo = async (e)=>{
+        e.preventDefault();
+        
+        try{
+            await userCollection().set({
+                todos: [...todos, {
+                    done:false,
+                    todo
+                }]
+            });
+            setTodo('');
+        }catch(e){
+            console.log(e)
+        }
     }
     return (
         <form onSubmit={addingTodo} className={styles["add"]} autoComplete="off">
             <input 
                 name="todo"
+                value={todo}
+                onChange={(e)=>setTodo(e.target.value)}
                 placeholder="What do you want to add" 
                 type="text"
             />
