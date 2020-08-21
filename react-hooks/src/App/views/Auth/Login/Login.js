8ec81097805a8661cddc00a881648b2752967058
LaupWing <loc_strict@hotmail.com>
2020-08-21 17:21:00 +0200

    login

--- react-hooks/src/App/views/Auth/Login/Login.js
+++ react-hooks/src/App/views/Auth/Login/Login.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
+import firebase from 'firebase';
 
 const Login = ({toggle}) => {
+    const [error, setError] = useState(null);
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        
+        try{
+            firebase
+                .auth()
+                .signInWithEmailAndPassword(email, password)
+        }catch(e){
+            setError(e.message);
+        }
+    }
+
     return (
-        <form className="login_form">
+        <form className="login_form" onSubmit={handleSubmit}>
             <input name='email' type="text" placeholder="Your Email..."/>
             <input name='password' type="password" placeholder="Your Password..."/>
-            <p className="error"></p>
+            {error && <p className="error">{error}</p>}
             <button type="submit">Submit</button>
             <p className="link" onClick={()=>toggle()}>No account? Sign up here!</p>
         </form>
--- react-hooks/src/App/views/Home/Home.js
+++ react-hooks/src/App/views/Home/Home.js
@@ -5,7 +5,6 @@ const Home = ({history}) => {
     if(!firebase.auth().currentUser){
         history.replace('/auth')
     }
-    console.log(firebase.auth().currentUser)
     return (
         <div>
             Home
