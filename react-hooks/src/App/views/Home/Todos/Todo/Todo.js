2589243f0eb386974907bbbff93d930d2d9571f2
LaupWing <loc_strict@hotmail.com>
2020-08-21 18:19:57 +0200

    update

--- react-hooks/src/App/views/Home/Todos/Todo/Todo.js
+++ react-hooks/src/App/views/Home/Todos/Todo/Todo.js
@@ -1,19 +1,20 @@
-import React from 'react'
+import React from 'react';
+import styles from './Todo.module.css';
 
 const Todo = ({todo}) => {
     return (
-        <div className="todo">
+        <div className={styles["todo"]}>
         <h2>{todo.todo}</h2>
         <div class="buttons">
-            <button class="done">
+            <button className={styles["done"]}>
                 <img src={require('../../../../assets/done.svg')} alt="toggle"/>
             </button>
-            <button class="delete">
+            <button className={styles["delete"]}>
                 <img src={require('../../../../assets/delete.svg')} alt="delete"/>
             </button>
         </div>
     </div>
-    )
+    );
 }
 
 export default Todo
