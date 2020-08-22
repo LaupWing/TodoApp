8e2538b379f1fe374fc653a55562bd7985cec2f7
LaupWing <loc_strict@hotmail.com>
2020-08-22 17:49:28 +0200

    update

--- react-redux-type/src/index.js
+++ react-redux-type/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './App/App';
 import * as serviceWorker from './serviceWorker';
 import userReducer from './store/reducers/user';
 import todosReducer from './store/reducers/todos';
