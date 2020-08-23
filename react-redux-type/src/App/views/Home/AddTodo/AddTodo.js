8a88de4747191633dff48919796a146c25aba621
LaupWing <loc_strict@hotmail.com>
2020-08-23 15:25:38 +0200

    refactor addtodoo

--- react-redux-type/src/App/views/Home/AddTodo/AddTodo.js
+++ react-redux-type/src/App/views/Home/AddTodo/AddTodo.js
@@ -1,25 +1,25 @@
 import React, {useState} from 'react';
 import styles from './AddTodo.module.css';
 
-const AddTodo = ({todos, userCollection}) => {
+const AddTodo = () => {
     const [todo, setTodo] = useState('');
     const addingTodo = async (e)=>{
-        e.preventDefault();
-        if(todos.find(x=>x.todo === todo)){
-            alert('Todo already exists');
-            return;
-        }
-        try{
-            await userCollection().set({
-                todos: [...todos, {
-                    done:false,
-                    todo
-                }]
-            });
-            setTodo('');
-        }catch(e){
-            console.log(e)
-        }
+        // e.preventDefault();
+        // if(todos.find(x=>x.todo === todo)){
+        //     alert('Todo already exists');
+        //     return;
+        // }
+        // try{
+        //     await userCollection().set({
+        //         todos: [...todos, {
+        //             done:false,
+        //             todo
+        //         }]
+        //     });
+        //     setTodo('');
+        // }catch(e){
+        //     console.log(e)
+        // }
     }
     return (
         <form onSubmit={addingTodo} className={styles["add"]} autoComplete="off">
